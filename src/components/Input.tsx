fc8e055da8eb070010048b88856ff313c104f5d3
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-03-15 16:24:51 -0300

    feat: add component input

---
+++ src/components/Input.tsx
@@ -0,0 +1,24 @@
+import { ComponentProps } from 'react'
+interface InputRootProps extends ComponentProps<'div'> {}
+interface InputPrefixProps extends ComponentProps<'div'> {}
+interface InputControlProps extends ComponentProps<'input'> {}
+
+export function Prefix(props: InputPrefixProps) {
+  return <div {...props} />
+}
+export function Control(props: InputControlProps) {
+  return (
+    <input
+      className="flex-1 border-o bg-transparent p-0 text-zinc-900 placeholder-zinc-600"
+      {...props}
+    />
+  )
+}
+export function Root(props: InputRootProps) {
+  return (
+    <div
+      className="flex mx-1 w-full items-center rounded-lg gap-2 border border-zinc-300 px-3 py-2 shadow-sm"
+      {...props}
+    />
+  )
+}

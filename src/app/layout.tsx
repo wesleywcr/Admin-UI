5e20b4b24be21d2dc3b688643153570c27979149
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-03-15 23:08:42 -0300

    fix: add class antialiased

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -11,11 +11,11 @@ export const metadata: Metadata = {
 }
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
-    <html lang="en">
+    <html lang="en" className="antialiased">
       <body className={inter.className}>
         <div className="grid min-h-screen grid-cols-app">
           <Sidebar />
-          <main className="px-4 pb-12 pt-8"> {children}</main>
+          <main className="px-8 pb-12 pt-8"> {children}</main>
         </div>
       </body>
     </html>

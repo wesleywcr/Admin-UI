00253fd829be636a9229f9d85557fc474c6f55b4
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-03-29 09:05:36 -0300

    feat: add component complete of file input

--- src/components/Form/FileInput/Root.tsx
+++ src/components/Form/FileInput/Root.tsx
@@ -1,7 +1,37 @@
-import { ComponentProps } from 'react'
+'use client'
 
+import {
+  ComponentProps,
+  createContext,
+  useContext,
+  useId,
+  useState,
+} from 'react'
 export type RootProps = ComponentProps<'div'>
 
-export function Root(props: RootProps) {
-  return <div {...props} />
+type FileInputContextType = {
+  id: string
+  files: File[]
+  onFilesSelected: (files: File[], multiple: boolean) => void
 }
+const FileInputContext = createContext({} as FileInputContextType)
+
+export function Root(props: RootProps) {
+  const id = useId()
+  const [files, setFiles] = useState<File[]>([])
+
+  function onFilesSelected(files: File[], multiple: boolean) {
+    if (multiple) {
+      setFiles((state) => [...state, ...files])
+    } else {
+      setFiles(files)
+    }
+  }
+  return (
+    <FileInputContext.Provider value={{ id, files, onFilesSelected }}>
+      <div {...props} />
+    </FileInputContext.Provider>
+  )
+}
+
+export const useFileInput = () => useContext(FileInputContext)
--- src/components/Form/FileInput/index.ts
+++ src/components/Form/FileInput/index.ts
@@ -2,5 +2,6 @@ import { Control } from './Control'
 import { Root } from './Root'
 import { Trigger } from './Trigger'
 import { ImagePreview } from './ImagePreview'
+import { FileList } from './FileList'
 
-export { Control, ImagePreview, Root, Trigger }
+export { Control, ImagePreview, Root, Trigger, FileList }

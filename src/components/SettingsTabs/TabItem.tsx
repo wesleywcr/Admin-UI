e07988211e0d29efa5331f65e2dbcdaf89db2a30
wesleywcr <wesley.rodrigues.wcr@gmail.com>
2024-05-30 22:35:05 -0300

    feat: add dark mode in components of settings tabs

--- src/components/SettingsTabs/TabItem.tsx
+++ src/components/SettingsTabs/TabItem.tsx
@@ -10,7 +10,7 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
   return (
     <Tabs.Trigger
       value={value}
-      className="relative group px-1 pb-4 text-sm font-medium text-zinc-500 hover:via-violet-700 data-[state=active]:text-violet-700 outline-none"
+      className="relative group px-1 pb-4 text-sm font-medium text-zinc-500 hover:via-violet-700 data-[state=active]:text-violet-700 outline-none dark:text-zinc-300 dark:hover:text-violet-300 dark:data-[state=active]:text-violet-300"
     >
       <span className="whitespace-nowrap group-focus-visible:ring-2 group-focus-visible:ring-violet-400 rounded group-focus-visible:ring-offset-4">
         {title}
@@ -18,7 +18,7 @@ export function TabItem({ value, title, isSelected = false }: TabItemProps) {
       {isSelected && (
         <motion.div
           layoutId="activeTab"
-          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 "
+          className="absolute -bottom-px left-0 right-0 h-0.5 bg-violet-700 dark:bg-violet-300"
         />
       )}
     </Tabs.Trigger>
--- src/components/SettingsTabs/index.tsx
+++ src/components/SettingsTabs/index.tsx
@@ -11,7 +11,7 @@ export function SettingsTabs() {
     <Tabs.Root value={currentTab} onValueChange={setCurrentTab}>
       <ScrollArea.Root className="w-full" type="scroll">
         <ScrollArea.Viewport className="w-full overflow-x-scroll">
-          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200">
+          <Tabs.List className="mt-6 flex w-full items-center gap-4 border-b border-zinc-200 dark:bg-zinc-900">
             <TabItem
               value={'tab1'}
               title="My details"
